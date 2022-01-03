0b59e7b68c8101f6565d53c3c208b6a8eb499c24
Sachin Kumar <42477579+pseudofool@users.noreply.github.com>
2022-01-03 21:58:50 +0530

    more functions added

--- Trees/tree_use.cpp
+++ Trees/tree_use.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 #include "tree_node.h"
+
+TreeNode<int>* takeInputLevelWise(){
+    int rootData;
+    cout << "Enter root data" << endl;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    while(pendingNodes.size() != 0){
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cout << "Enter the num of child of " << front->data << endl;
+        cin >> numChild;
+        for (int i = 0; i<numChild; i++){
+            int childData;
+            cout << "Enter the " << i << "th child of " << front->data << endl;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            pendingNodes.push(child);
+            front->children.push_back(child);
+        }
+    }
+    return root;
+
+}
+
 
 TreeNode<int>* takeInput(){
     int rootData;
@@ -16,6 +45,22 @@ TreeNode<int>* takeInput(){
     }
     return root;
 }
+
+void printTreeLevelWise(TreeNode<int>* root){
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    while(pendingNodes.size() != 0){
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data << ": ";
+        for (int i=0; i<front->children.size(); i++){
+            pendingNodes.push(front->children[i]);
+            cout << front->children[i]->data << ", ";
+        }
+        cout << endl;
+    }
+}
+
 void printTree(TreeNode<int>* root){
     cout << root->data << ": ";
     for (int i=0; i<root->children.size(); i++){
@@ -32,8 +77,8 @@ int main(){
     TreeNode<int>* node1 = new TreeNode<int>(2);
     TreeNode<int>* node2 = new TreeNode<int>(3);
     */
-    TreeNode<int>* root = takeInput();
-    printTree(root);
+    TreeNode<int>* root = takeInputLevelWise();
+    printTreeLevelWise(root);
     
 
 
