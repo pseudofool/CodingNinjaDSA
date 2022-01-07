b5f9dc81742c4d07fbe4b703aff661bed8594c42
Sachin Kumar <42477579+pseudofool@users.noreply.github.com>
2022-01-07 16:18:08 +0530

    print level wise added

--- Binary_Trees/binary_tree_use.cpp
+++ Binary_Trees/binary_tree_use.cpp
@@ -71,6 +71,29 @@ void printTree(BinaryTreeNode<int>* root){
     printTree(root->right);
 }
 
+void printLevelWise(BinaryTreeNode<int> *root) {
+    queue<BinaryTreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    while(pendingNodes.size() != 0){
+        BinaryTreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        
+        cout << front->data << ":";
+        if (front->left != NULL){
+            cout << "L:" << front->left->data << ",";
+            pendingNodes.push(front->left);
+        }else{
+            cout << "L:" << -1 << ",";
+        }
+        if (front->right != NULL){
+            cout << "R:" << front->right->data;
+            pendingNodes.push(front->right);
+        }else{
+            cout << "R:" << -1;
+        }
+        cout << endl;
+    }
+}
 
 int main(){
     /*BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
