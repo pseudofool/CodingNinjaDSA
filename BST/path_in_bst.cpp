4e0af13c973327585e59e5fcc15f3d32fa38b2a2
Sachin Kumar <42477579+pseudofool@users.noreply.github.com>
2022-01-16 13:02:12 +0530

    path in binary tree and bst added

---
+++ BST/path_in_bst.cpp
@@ -0,0 +1,29 @@
+vector<int>* getPath(BinaryTreeNode<int> *root , int data) {
+	if (root == NULL){
+        return NULL;
+    }
+    if (root->data == data){
+        vector<int>* output = new vector<int>();
+        output->push_back(root->data);
+        return output;
+    }
+    
+    if(root->data > data){
+        vector<int>* leftOutput = getPath(root->left, data);
+        if (leftOutput != NULL){
+            leftOutput->push_back(root->data);
+        }
+        return leftOutput;
+        
+    }
+    else if (root->data < data){
+        vector<int>* rightOutput = getPath(root->right, data);
+        if (rightOutput != NULL){
+            rightOutput->push_back(root->data);
+        }
+        return rightOutput;
+    }else{
+        return NULL;
+    }
+}
+ 
